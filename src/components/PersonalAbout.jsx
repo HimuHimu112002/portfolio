ee9aa97fcaa52fa3d09018f105809cdc9e6f580a
HimuHimu112002 <mdhmaktaruzzaman9101@gmail.com>
2024-02-24 18:17:54 +0600

    update

--- src/components/PersonalAbout.jsx
+++ src/components/PersonalAbout.jsx
@@ -1,26 +1,15 @@
 import { Col, Container, Row} from 'react-bootstrap'
-import {Zoom} from "react-awesome-reveal";
-import { useEffect } from 'react';
-import AOS from 'aos';
-import Skill from '../components/Skill'
 const PersonalAbout = () => {
-    useEffect(()=>{
-        AOS.init({
-            duration: 1000,
-            easing: 'ease-in-sine',
-          });
-      },[])
-
     let aboutText = 'First, take my salam. I am Aktarujjaman and running student of CSE. I am  a professional (MERN STACK) developer. I love taking on new challenges and solving problems. I can easily communicate with my all team members and I can adapt myself to any environment. My main focus is improving my MERN technology and programming skill.And I am a quick learner of any new technology.'
 
 return (
-    <Container className="py-5" data-aos="fade-right">
+    <Container className="py-5">
         <Row>
             <div className='py-5 about_heading'>
                 <h1>ABOUT ME</h1> 
             </div>
 
-            <Col data-aos="fade-up-right" sm={10} md={10} className='m-auto mb-5'>
+            <Col sm={10} md={10} className='m-auto mb-5'>
                 <div className='about_content'>
                     <div className='mt-2'>
                         <h5 className='about_subHeading text-white'>{aboutText}</h5>
@@ -65,7 +54,6 @@ return (
                             </Col>
                         </Row>
                     </div>
-                    {/* <img className='img-fluid signature_img' src='image/signature.png'></img> */}
                 </div>
             </Col>
         </Row>
